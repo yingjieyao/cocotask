fc42fe59101aa13674fa4a0258a9a19e9b08985c
apache <apache@tencent.com>
2018-09-26 10:05:18 +0800

    up

--- cocotask/rabbitmq/common.py
+++ cocotask/rabbitmq/common.py
@@ -1,16 +1,17 @@
 import pika
 
 def createBlockingConnection(config):
-    credentials = pika.PlainCredentials(config['USERNAME'], config['PASSWORD'])
-    parameters = pika.ConnectionParameters(
-        config['SERVER_ADDRESS'],
-        config['SERVER_PORT'],
-        config.get("VIRTUAL_HOST", "/"),
-        credentials,
-        heartbeat_interval=0)
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
-    channel.exchange_declare(exchange=config['EXCHANGE_NAME'], exchange_type=config['EXCHANGE_TYPE'])
-    channel.queue_declare(queue=config['QUEUE_NAME'], durable=True)
+	credentials = pika.PlainCredentials(config['USERNAME'], config['PASSWORD'])
+	parameters = pika.ConnectionParameters(config['SERVER_ADDRESS'],
+	                                   config['SERVER_PORT'],
+	                                   config.get("VIRTUAL_HOST", "/"),
+	                                   credentials,
+	                                   heartbeat=config.get("HEARTBEAT", 0))
+	connection = pika.BlockingConnection(parameters)
+	channel = connection.channel()
 
-    return connection, channel
+	channel.exchange_declare(exchange=config['EXCHANGE_NAME'],
+	                     exchange_type=config['EXCHANGE_TYPE'])
+	channel.queue_declare(queue=config['QUEUE_NAME'], durable=True)
+
+	return connection, channel
