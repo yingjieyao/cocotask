cbded24f202d47b2c0564c7ee9bef0810fa469f2
likezhang <likezhang@tencent.com>
2018-10-16 20:21:51 +0800

    consumer should not exit

--- cocotask/consumer_manager.py
+++ cocotask/consumer_manager.py
@@ -3,6 +3,7 @@ from .rabbitmq.rmq_consumer import CocoRMQConsumer
 from .kafka.kafka_consumer import CocoKafkaConsumer
 from .redis.redis_consumer import CocoRedisConsumer
 from .logger import logger
+import time
 
 
 # this is aka mixture of a consumer factory and threadpool
@@ -38,8 +39,14 @@ class CocoConsumerManager(object):
         consumer_type = config["MQ_TYPE"]
         logger.debug('cusumer type: {}'.format(consumer_type))
         consumer_class = CocoConsumerManager.CONSUMER_CLASS[consumer_type]
-
         sub_config = config[consumer_type]
-        consumer = consumer_class(sub_config, worker, logger)
-        consumer.connect()
-        logger.warning("Consumer %d stopped" % seq)
+
+        while True:
+            try:
+                consumer = consumer_class(sub_config, worker, logger)
+                consumer.connect()
+            except Exception as err:
+                    logger.error(err)
+                    logger.warning("Consumer Error. Reconnect after 10 seconds.")
+                    time.sleep(10)
+
