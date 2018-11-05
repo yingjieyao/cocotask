fa097b2a963f200e483060214e1c8687c3a5f923
likezhang <likezhang@tencent.com>
2018-11-05 16:13:18 +0800

    create new work object for each message to avoid memory problem

---
+++ test/consumer_test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from cocotask import CocoConsumerManager as CCManager
+from cocotask import CocoBaseWorker
+import time
+
+from jsmin import jsmin
+import json
+
+class TestWorker(CocoBaseWorker):
+    def process(self, body):
+        arr = []
+        for i in range(1,1000*1000*100):
+            arr.append(i)
+        time.sleep(10)
+        print("done")
+
+with open('config.json', 'r') as f:
+    config = json.loads(jsmin(f.read()))
+
+manager = CCManager(config, TestWorker, 4)
+manager.start()
+
